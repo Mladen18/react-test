f580e1ba3532cd8561284d046e5831d176a779db
Mladen Krznaric <mladen@q.agency>
2022-02-07 18:03:07 +0100

    test mock

--- src/pages/PostList.test.tsx
+++ src/pages/PostList.test.tsx
@@ -9,28 +9,28 @@ describe("PostList component", () => {
     expect(searcText).toBeInTheDocument();
   });
 
-  test("renders posts if request succeeds", async () => {
-    // window.fetch = jest.fn();
-    // (window.fetch as jest.Mock).mockResolvedValueOnce({
-    //   json: async () => [
-    //     {
-    //       userId: 1,
-    //       id: 1,
-    //       title: "Title",
-    //       body: "Morem text",
-    //     },
-    //     {
-    //       userId: 1,
-    //       id: 2,
-    //       title: "Title2",
-    //       body: "Lorem",
-    //     },
-    //   ],
-    // });
-    render(<PostsList message={""} />);
+  // test("renders posts if request succeeds", async () => {
+  // window.fetch = jest.fn();
+  // (window.fetch as jest.Mock).mockResolvedValueOnce({
+  //   json: async () => [
+  //     {
+  //       userId: 1,
+  //       id: 1,
+  //       title: "Title",
+  //       body: "Morem text",
+  //     },
+  //     {
+  //       userId: 1,
+  //       id: 2,
+  //       title: "Title2",
+  //       body: "Lorem",
+  //     },
+  //   ],
+  // });
+  // render(<PostsList message={""} />);
 
-    const postItems = await screen.findAllByRole("listitems");
-    // console.log(postItems);
-    expect(postItems).not.toHaveLength(0);
-  });
+  // const postItems = await screen.findAllByRole("listitems");
+
+  // expect(postItems).not.toHaveLength(0);
+  // });
 });
