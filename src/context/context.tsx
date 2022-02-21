2600c0479a98b46dd2511f39251e92fdbceae2e0
Mladen Krznaric <mladen@q.agency>
2022-02-21 10:15:16 +0100

    caching stale

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import { createContext, useContext } from "react";
 import { Post, User, Comment } from "../interface/index";
 import fetchData from "../components/hooks/fetch-query";
 import { useQuery, UseQueryResult } from "react-query";
@@ -11,11 +11,11 @@ interface All {
 const PostContext = createContext<All>(undefined!);
 
 export function PostContextProvider({ children }: { children: React.ReactNode }) {
-  // const [usePost, setPost] = useState<Post | undefined>(undefined);
-
-  const { data, status }: UseQueryResult<undefined> = useQuery(["data", null], () => fetchData(null, "2"));
+  const { data, status }: UseQueryResult<undefined> = useQuery(["data", null], () => fetchData(null, "2"), {
+    staleTime: 10000,
+  });
 
   return <PostContext.Provider value={{ data, status }}>{children}</PostContext.Provider>;
 }
 
-export const useData = () => React.useContext(PostContext);
+export const useData = () => useContext(PostContext);
